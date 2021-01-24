10e6938a799a89067b29c0d8261bbf699865b163
wyychelsea <wyychelsea@gmail.com>
2021-01-25 02:18:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,6 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+git clone https://github.com/vernesong/OpenClash.git
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.22.1/g' package/base-files/files/bin/config_generate
